9d26c062c362de6597b34c4cb8a14f7f126bcc31
Ayaal Kaplin <uraankhayayaal@gmail.com>
2022-04-09 14:18:53 +0900

    add error message and disabled state

--- checkbox/WCheckbox.php
+++ checkbox/WCheckbox.php
@@ -8,12 +8,14 @@ class WCheckbox extends \yii\base\Widget
 {
     public $model;
     public $attribute;
+    public $disabled = false;
 
     public function run()
     {
         return $this->render('index', [
             'model' => $this->model,
             'attribute' => $this->attribute,
+            'disabled' => $this->disabled,
         ]);
     }
 }
--- checkbox/views/index.php
+++ checkbox/views/index.php
@@ -3,10 +3,15 @@
 use yii\helpers\Html;
 
 ?>
+	<div class="form-group <?= !empty($model->getErrors($attribute)) ? 'red-text' : '' ?>">
 		<p>
 		    <label>
-		    <input type="hidden" name="<?= $model->formName() ?>[<?= $attribute ?>]" value="0" />
-		    <input type="checkbox" id="<?= Html::getInputId($model, $attribute); ?>" name="<?= $model->formName() ?>[<?= $attribute ?>]" <?= ($model->$attribute) ? 'checked' : ''; ?>  value="1">
+		    <input type="hidden" name="<?= $model->formName() ?>[<?= $attribute ?>]" value="0" <?= ($disabled == 'disabled') ? 'disabled' : ''; ?> />
+		    <input type="checkbox" id="<?= Html::getInputId($model, $attribute); ?>" name="<?= $model->formName() ?>[<?= $attribute ?>]" <?= ($model->$attribute) ? 'checked' : ''; ?> <?= ($disabled == 'disabled') ? 'disabled' : ''; ?> value="1">
 		    <span for="<?= Html::getInputId($model, $attribute); ?>"><?= $model->getAttributeLabel($attribute); ?></span>
 		    </label>
 		</p>
+		<?php foreach ($model->getErrors($attribute) as $key => $error) { ?>
+			<p class="help-block"><?= $error ?></p>
+		<?php } ?>
+	</div>
